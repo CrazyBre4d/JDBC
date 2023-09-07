d2196fe7d397f56b0edfd4b693edd883b396353e
Vlas <vpkabakov@gmail.com>
2023-09-07 15:57:49 +0300

    3rd Commit

--- src/main/java/vlas/Main.java
+++ src/main/java/vlas/Main.java
@@ -9,9 +9,16 @@ Statement
 PreparedStatement
 callablestatement
 
+слоистая архитектура
 
-!!!!добавление редактирование просмотр вьюшка заказа
-!!! сделать так кабы динамически обновлялись таблицы в командной строке
+ md5
+ entity model
+
+ connection pull
+
+ property file ( настройки бд )
+
+!!!!добавление редактирование, просмотр, вьюшка заказов
 View в базе данных
 и не только читать
 а реализовывать программу
@@ -23,44 +30,89 @@ public class Main {
         Connection conn = null;
         Statement st = null;
         ResultSet rs = null;
-
-
-
-        Class.forName("oracle.jdbc.driver.OracleDriver");
-        conn = DriverManager.getConnection("jdbc:oracle:thin:@scomplat.scx:1521:komplat","kabakov","kabakov");
-
-
         Scanner sc= new Scanner(System.in);
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+
+        System.out.println("Добрый день. Введи имя и пароль для входа в БД:");
+        String user = sc.nextLine();
+        String password = sc.nextLine();
+        conn = DriverManager.getConnection("jdbc:oracle:thin:@scomplat.scx:1521:komplat",user,password);
+
+
+        System.out.println("Добро пожаловать в базу данных ОАО 'В ГОСТЯХ У ВЛАСА' \nСалам, Женя! \n Выберите таблицу или действие:\n" +
+                "1 - Users  \n2 - Roles \n3 - Product \n4 - Product_type \n5 - Orders \n6 -Просмотр заказов ");
         int operator = sc.nextInt();
-        System.out.println("Добро пожаловать в базу данных ОАО 'В ГОСТЯХ У ВЛАСА' \n Салам, Женя!\n Выберите доступную операцию:\n" +
-                "1 - Просмотр таблиц \n2 - Удаление   ");
         switch (operator) {
             case 1:
-
+                st = conn.createStatement();
+                rs = st.executeQuery("SELECT * FROM USERSS ");
+                while (rs.next()) {
+                    System.out.println(rs.getInt(1) + " " + rs.getString(2) + " "
+                            + rs.getString(3) + " " + rs.getInt(4) + " " + rs.getString(5) + " " + rs.getString(6));
+                }
+                System.out.println("Выберите операцию: \n1 - Добавление сущности \n2 - Удаление сущности \3 ");
                 break;
             case 2:
+                st = conn.createStatement();
+                rs = st.executeQuery("SELECT * FROM ROLESS ");
+                while (rs.next()) {
+                    System.out.println(rs.getInt(1) + " " + rs.getString(2));
+                }
 
                 break;
             case 3:
+                st = conn.createStatement();
+                rs = st.executeQuery("SELECT * FROM PRODUCT ");
+                while (rs.next()) {
+                    System.out.println(rs.getInt(1) + " " + rs.getString(2) + " "
+                            + rs.getInt(3) + " " + rs.getString(4) + " " + rs.getInt(5));
+                }
 
                 break;
             case 4:
+                st = conn.createStatement();
+                rs = st.executeQuery("SELECT * FROM PRODUCT_TYPE ");
+                while (rs.next()) {
+                    System.out.println(rs.getInt(1) + " " + rs.getString(2) );
+                }
                 break;
+
+            case 5:
+                st = conn.createStatement();
+                rs = st.executeQuery("SELECT * FROM ORDERS ");
+                while (rs.next()) {
+                    System.out.println(rs.getInt(1) + " " + rs.getInt(2) + " "
+                            + rs.getInt(3) + " " + rs.getInt(4) + " " + rs.getString(5));
+                }
+                System.out.println("Выберите операцию: \n1 - Добавление сущности \n2 - Удаление сущности \3 ");
+                int input = sc.nextInt();
+
+                switch (input){
+                    case 1:
+                String sql = "INSERT INTO ORDERS (ORDER_ID, USER_ID, PRODUCT_ID, IS_ORDERED, PURCHASE_DATE) " +
+                        "values ( 4, 1, 1, 1, '10.11.2023')";
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ps.executeUpdate();
+
+                break;
+                    case 2:
+                }
+
+            case 6:
+
+                break;
+
             default:
-                throw new IllegalArgumentException("Enter number from 1 to 4!");
+                throw new IllegalArgumentException("Enter number from 1 to 7!");
         }
 
 
-        st = conn.createStatement();
-        rs = st.executeQuery("SELECT * FROM USERSS");
 
 
 
-        while (rs.next()) {
-            System.out.println(rs.getInt(1) + " " + rs.getString(2) + " "
-                    + rs.getString(3) + " " + rs.getInt(4) + " " + rs.getString(5) + " " + rs.getString(6));
-        }
-        conn.close();
+
+
+
 
 
 
--- src/main/java/vlas/OracleConnection.java
+++ src/main/java/vlas/OracleConnection.java
@@ -1,10 +1,25 @@
 package vlas;
 
-import java.sql.PreparedStatement;
+import java.sql.*;
 
-public class OracleConnection {
+public class  OracleConnection {
+    private String command;
 
-/*    String sql = "INSERT INTO USERSS (user_id, f_name, l_name, role_id, loginn, passwordd) " +
+    public OracleConnection(String command) {
+        this.command = command;
+    }
+
+    public  void  getTable( String name ) throws SQLException {
+
+      /*  Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery(name);
+        while (rs.next()) {
+            System.out.println(rs.getInt(1) + " " + rs.getString(2) + " "
+                    + rs.getString(3) + " " + rs.getInt(4) + " " + rs.getString(5) + " " + rs.getString(6));
+        }*/
+    }
+
+    /*    String sql = "INSERT INTO USERSS (user_id, f_name, l_name, role_id, loginn, passwordd) " +
             "values (2, 'Vlas3', 'Kabakov', 1, 'vlas1', 'vlas')";
     PreparedStatement ps = conn.prepareStatement(sql);
         ps.executeUpdate();*/
