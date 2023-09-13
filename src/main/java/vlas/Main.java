12c08b9eebb4609a11f39051dbfc9ff9bc69cf39
Vlas <vpkabakov@gmail.com>
2023-09-13 11:53:31 +0300

    minifix v2.0.2

--- src/main/java/vlas/Main.java
+++ src/main/java/vlas/Main.java
@@ -2,7 +2,10 @@ package vlas;
 
 import vlas.services.Menu;
 
-/*меню для пользователя, получение пользователя */
+/*меню для пользователя, получение пользователя
+*
+* можно сделаь один поиск по двум параметрам
+* */
 public class Main {
     public static void main(String[] args)  {
         Menu menu = new Menu();
